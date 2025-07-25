c06d58591c8aa1ba68bc9421631fa5707897cffb
leoQM <theleox12@gmail.com>
2025-07-25 10:54:43 -0500

    change file server3

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,6 @@
 import { CommonEngine } from '@angular/ssr/node'
   import { render } from '@netlify/angular-runtime/common-engine.mjs'
-​
   const commonEngine = new CommonEngine()
-​
   export async function netlifyCommonEngineHandler(request: Request, context: any): Promise<Response> {
     // Example API endpoints can be defined here.
     // Uncomment and define endpoints as necessary.
@@ -10,6 +8,5 @@ import { CommonEngine } from '@angular/ssr/node'
     // if (pathname === '/api/hello') {
     //   return Response.json({ message: 'Hello from the API' });
     // }
-​
     return await render(commonEngine)
   }
