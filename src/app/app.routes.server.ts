68bf984ea1efb19c68b78a457c8a8a1d1e988850
leoQM <theleox12@gmail.com>
2025-07-25 15:32:46 -0500

    getparam

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -5,5 +5,31 @@ export const serverRoutes: ServerRoute[] = [
     path: '**',
     renderMode: RenderMode.Server
   },
+  {
+    path: 'product/:id',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      // Aquí debes devolver una lista de valores para ":id"
+      return [
+        { id: '1' },
+        { id: '2' },
+        { id: '3' },
+        { id: '4' },
+        { id: '5' },
+        { id: '6' },
+        { id: '7' },
+        { id: '8' },
+        { id: '9' },
+        { id: '10' },
+        { id: '11' },
+        { id: '12' },
+        { id: '13' },
+        { id: '14' },
+        { id: '15' },
+        { id: '16' },
+        { id: '17' },
+      ];
+    }
+  }
 
 ];
